6f23d051d6b0594c60c160fd61b8bae7161ecb4b
Francisco Reinolds <francisco.reinolds@outlook.pt>
2020-06-27 14:19:33 +0100

    Full Black Footer. (#51)

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -2,17 +2,72 @@
     <div>
         <v-footer
         class = "footer"
-        color = "grey"
-        height = 70px
-        max-height = 70px
+        color = "black"
+        height = 150px
+        max-height = 150px
         >
-        <p class="justify-center">Center aligned text on all viewport sizes.</p>
+            <v-card
+            class = "my_card"
+            height = "120px"
+            min-width = "200px"
+            color = "black"
+            >
+                <v-card-text
+                class = "my_card_text"
+                >
+                    <v-btn
+                    v-for="icon in icons"
+                    :key = "icon.index"
+                    class = "mx-4"
+                    dark
+                    icon
+                    >
+                        <v-icon
+                        dark
+                        size = "24px"
+                        >
+                            {{icon}}
+                        </v-icon>
+                    </v-btn>
+                </v-card-text>
+                <v-divider></v-divider>
+                <v-card-text
+                class = "text-center white--text"
+                >
+                    {{ new Date().getFullYear() }} - <strong>FreeTours</strong>
+                </v-card-text>
+            </v-card>
         </v-footer>
     </div>
 </template>
 
 <script>
 export default {
-    name : "Footer"
+    name : "Footer",
+    data: () => ({
+        icons : [
+            'mdi-facebook',
+            'mdi-twitter',
+            'mdi-email'
+        ]
+    }),
 }
+
+
+
 </script>
+
+<style>
+
+<link href="https://cdn.jsdelivr.net/npm/@mdi/font@5.x/css/materialdesignicons.min.css" rel="stylesheet">
+
+.footer {
+    justify-content : center;
+}
+
+.my_card {
+    margin : 0 auto;
+    justify-text : center;
+}
+
+</style>
