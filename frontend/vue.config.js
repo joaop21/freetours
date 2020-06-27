67aebc7cd80f73d571e9e8f66bdd0fad9d5bca2b
Francisco Reinolds <francisco.reinolds@outlook.pt>
2020-06-27 14:20:05 +0100

    DevServer removed (#49)

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,13 +1,4 @@
 module.exports = {
-    devServer : {
-        proxy : {
-            '/' : {
-                target : 'localhost:8081/',
-                ws : true,
-                changeOrigin : true
-            }
-        }
-    },
     "transpileDependencies": [
         "vuetify"
     ]
