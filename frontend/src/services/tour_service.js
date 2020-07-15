af039ebb248686c7aa7a780f0567670dc8c01282
Diogo Braga <diogofiliperfbraga@gmail.com>
2020-07-15 21:34:49 +0100

    CreateTour - Frontend (#110)

--- frontend/src/services/tour_service.js
+++ frontend/src/services/tour_service.js
@@ -26,6 +26,44 @@ class TourService {
             });
     }
 
+    createTour(tour) {
+
+        // Duration
+        if(tour.duration_hours.length==1) tour.duration_hours = "0".concat(tour.duration_hours);
+        if(tour.duration_minutes.length==1) tour.duration_minutes = "0".concat(tour.duration_minutes);
+        tour.duration = tour.duration_hours.concat(":",tour.duration_minutes);
+
+        const auth = localStorage.getItem('user')
+        let config = {};
+
+        if (auth) {
+            config = {
+                headers: {
+                  Authorization: auth,
+                }
+            }
+        }
+
+        console.log("Duration " + tour.duration)
+        console.log("Loc: " + tour.location);
+
+        return axios
+            .post(API_URL + '/createTour', {
+                name: tour.name,
+                description: tour.description,
+                duration: tour.duration,
+                location: tour.location
+            }, config)
+            .then(response => {
+                console.log("Pedido efetuado com sucesso");
+                return response;
+            })
+            .catch(error => {
+                console.log("Erro");
+                return error.response;
+            });
+    }
+
 }
 
 export default new TourService();
